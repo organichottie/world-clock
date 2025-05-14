cdee53d934aef01beedca46d3d0c717dc51432b9
organichottie <mmatlitech@gmail.com>
2025-05-14 06:31:23 +0200

    drop down menu and timezone improved.
    When selecting city, info of the city and timezone displayed

--- javascript/index.js
+++ javascript/index.js
@@ -33,5 +33,27 @@ function updateTime() {
     .format("h:mm:ss [<small>]A[</small>]");
 }
 
+function updateCity(event) {
+  event.preventDefault();
+  let cityTimezone = event.target.value;
+  let cityName = cityTimezone.replace("_", " ").split("/")[1];
+  let cityTime = moment.tz(cityTimezone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+  <div>
+    <h2>${cityName}</h2>
+    <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+  </div>
+  <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+</div>
+`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let selectedCityElement = document.querySelector("#city");
+selectedCityElement.addEventListener("change", updateCity);
